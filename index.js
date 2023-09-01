0d22758c41a088aacd912068412937e3f7ce82ac
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-09-01 11:49:23 +0600

    sorting done

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let sorted  = false;
+let vidId = 1000;
+let dataLength=0;
 // blog button
 
 const blog = () => {
@@ -29,87 +32,87 @@ const category = async () => {
 category()
 
 
-
-// all
-
-const videos = async () => {
-    const video = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
-    const cate = await video.json();
-    const all = cate.data;
-    // console.log(all);
-   
-
-    const allCategory = document.getElementById('all-cat');
-    all.forEach(vid => {
-        // const author=vid.authors
-        pic=vid.authors.map(aut=>`<p>${aut}</p>`).join("")
-        // console.log(pic)
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card h-96 bg-base-100 shadow-xl">
-  <figure><img class="h-48 w-auto rounded-lg" src="${vid.thumbnail}" /></figure>
-  <div class="card-body">
-   <img src="${vid.authors.profile_picture}"/>
-    <h2 class="card-title">
-      ${vid.title}
-     
-    </h2>
-     ${pic.profile_name}
-    <p>${vid.others.views} <span>views</span></p>
-  </div>
-</div>
-        `
-        allCategory.appendChild(div)
-    })
-
-}
-
-videos()
-
 // all category
 
-const cats =async (id)=>{
+const cats =async (id=1000)=>{
     const vide = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const catego = await vide.json();
     const alll = catego.data;
-    // console.log(all);
+    dataLength=alll.length;
+   hub(alll)
+}
+    
    
 
+const hub=(alll)=>{
     const alllCategory = document.getElementById('all-cat');
     alllCategory.textContent='';
+    if(sorted){
+        alll.sort((a,b)=>{
+            const strA=parseInt(a.others.views);
+            const strB=parseInt(b.others.views);
+            return strB-strA;
+
+        })
+    }
+   if(dataLength){
     alll.forEach(vido => {
         const img=vido.authors.map((im)=>im.profile_picture)
         const pics=vido.authors.map((aut)=>aut.profile_name)
+        // const badge=vido.authors.map((bad)=>bad?.verified icon:)
+        // ${vido?'<img class="w-[]"src="images/svg.png">':''}
+        
         
         console.log(pics)
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="card h-96 bg-base-100 shadow-xl">
+        <div class="card h-96 bg-base-100 ">
   <figure><img class="h-48 w-auto rounded-lg" src="${vido.thumbnail}" /></figure>
   <div class="card-body">
-   <div class="flex flex-row items-center gap-4"><img class=" w-[50px] h-[50px] rounded-full" src="${img}"/>
+   <div class="flex flex-row items-center gap-4"><img class=" w-[40px] h-[40px] rounded-full" src="${img}"/>
    <h2 class="card-title">
      ${vido.title}
     
    </h2></div>
      <p class="ml-16">${pics}</p>
+     
     <p class="ml-16">${vido.others.views} <span>views</span></p>
   </div>
 </div>
         `
         alllCategory.appendChild(div)
     })
+   }
+   else{
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <h2>No data found</h2>
+    `
+    alllCategory.appendChild(div)
+   }
 }
+
 cats()
 
 
 
 const vidCat =  (id)=>{
     // console.log(id)
-    cats(id)
-    
-       
-    
-    
+    vidId=id;
+    cats(vidId) 
 }
 vidCat()
+
+
+// sorting
+
+const sort =async (id)=>{
+    console.log('click')
+    sorted = true
+    const vide = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const catego = await vide.json();
+    const alll = catego.data;
+    hub(alll)
+    cats(vidId)
+}
+// sort()
